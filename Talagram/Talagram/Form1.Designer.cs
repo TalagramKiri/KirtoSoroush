260d02a5123076b00c11f1b8580ed673658a5924
Alishoooo Esteres <alishoooo.esteres@outlook.com>
2018-06-10 02:04:20 +0430

    kir to Soroush

---
+++ Talagram/Talagram/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Talagram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(223, 59);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(300, 49);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "از این پروکسی ها با خیال راحت تو تلگرام نسخه ی اصلی استفاده کنید. و برینید به کسا" +
+    "یی که تلگرام رو فیلتر کردن و میخوان مردم رو به زور به سمت هات گرام تخمی ببرن";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 13);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Link for telegram";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Pass";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "User";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Port";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Server";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(103, 116);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(420, 20);
+            this.textBox5.TabIndex = 17;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(59, 88);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(158, 20);
+            this.textBox4.TabIndex = 16;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(59, 62);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(158, 20);
+            this.textBox3.TabIndex = 15;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(59, 36);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(158, 20);
+            this.textBox2.TabIndex = 14;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(158, 20);
+            this.textBox1.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(223, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(300, 46);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Get new Proxy";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_ClickAsync);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 146);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Talagram/Talagram/Form1.cs
@@ -0,0 +1,112 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Talagram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static String DecryptIt(String s, byte[] key, byte[] IV)
+        {
+            String result;
+
+            RijndaelManaged rijn = new RijndaelManaged();
+            rijn.Mode = CipherMode.CBC;
+            rijn.Padding = PaddingMode.Zeros;
+
+            using (MemoryStream msDecrypt = new MemoryStream(Convert.FromBase64String(s)))
+            {
+                using (ICryptoTransform decryptor = rijn.CreateDecryptor(key, IV))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader swDecrypt = new StreamReader(csDecrypt))
+                        {
+                            result = swDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            rijn.Clear();
+
+            return result;
+        }
+
+        private async void button1_ClickAsync(object sender, EventArgs e)
+        {
+            var info = await GetInfo();
+            Encoding byteEncoder = Encoding.Default;
+
+            byte[] rijnKey = byteEncoder.GetBytes("KCH@LQj#>6VCqqLg");
+            byte[] rijnIV = byteEncoder.GetBytes("YC'2bmK=b%#NQ?9j");
+            string proxyjson = DecryptIt(info.data[0], rijnKey, rijnIV);
+
+            int index = proxyjson.IndexOf("}");
+            index++;
+            if (index > 0)
+                proxyjson = proxyjson.Substring(0, index);
+
+
+            var cls = JsonConvert.DeserializeObject<Proxy>(proxyjson);
+
+            textBox1.Text = cls.ip;
+            textBox2.Text = cls.prt.ToString();
+            textBox3.Text = cls.usr;
+            textBox4.Text = cls.pwd;
+            textBox5.Text = "https://t.me/socks?server=" + cls.ip + "&port=" + cls.prt + "&user=" + cls.usr + "&pass=" + cls.pwd;
+        }
+        public async Task<INFO> GetInfo()
+        {
+            return await Task.Run(() =>
+            {
+                try
+                {
+                    string url = "http://lh58.hotgram.ir/v1/proxy?slt=77799000&appId=3";
+                    HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+                    string json = "";
+                    using (HttpWebResponse response = req.GetResponse() as HttpWebResponse)
+                    {
+                        StreamReader reader = new StreamReader(response.GetResponseStream());
+                        json = reader.ReadToEnd();
+                    }
+                    INFO cls = JsonConvert.DeserializeObject<INFO>(json);
+                    return cls;
+                }
+                catch (Exception ex)
+                {
+                    INFO iNFO = new INFO();
+                    return iNFO;
+                }
+            });
+        }
+
+        public class INFO
+        {
+            public List<string> data { get; set; }
+        }
+
+        public class Proxy
+        {
+            public string ip { get; set; }
+            public int prt { get; set; }
+            public string usr { get; set; }
+            public string pwd { get; set; }
+            public int ttl { get; set; }
+        }
+    }
+}
